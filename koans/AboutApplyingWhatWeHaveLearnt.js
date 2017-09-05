69bda9f9115e7a5154a140d29e77232acbcf654d
Patrick Miner <patrickminer318@gmail.com>
2017-09-04 20:29:04 -0700

    Passed AboutApplyingWhatWeHaveLearnt.js specs

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -30,15 +30,20 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      var pizzas = products.filter(function(pizza) {
+        return pizza.ingredients.includes('mushrooms') === false;
+      }).filter(function(pizza) {
+        return pizza.containsNuts === false;
+      })
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(pizzas.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -52,13 +57,20 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(sum);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _.range(1000).reduce((sum, number) => {
+      if (number % 3 === 0 || number % 5 === 0) {
+        return sum + number;
+      }
+      else {
+        return sum;
+      }
+    }, 0);    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -71,20 +83,26 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
+    
+    products.forEach((pizza) => {
+      pizza.ingredients.forEach((ingredient) => {
+        ingredientCount[ingredient] = ingredientCount[ingredient] + 1 || 1;
+      });
+    });
 
     /* chain() together map(), flatten() and reduce() */
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
-  /*
+
   it("should find the largest prime factor of a composite number", function () {
   
   });
@@ -105,5 +123,4 @@ describe("About Applying What We Have Learnt", function() {
   it("should find the 10001st prime", function () {
 
   });
-  */
 });
